21b06f80fcc2337087aa6d5843751265971f2b6a
GillesDuvert <gilles.duvert@free.fr>
2024-06-28 17:50:22 +0200

    added options to simple test CPP file

--- src/antlr4/main.cpp
+++ src/antlr4/main.cpp
@@ -5,22 +5,55 @@ int inloop=0;
 #include "gdlParser.h"
 
 using namespace antlr4;
+using namespace std;
 
-int main(int , const char **) {
-
+int main(int argc, char *argv[]){
+    bool showtree=false;
+    bool showtokens=false;
+    bool token_only=false;
+    for( auto a=1; a< argc; ++a)
+    {
+      if( string( argv[a]) == "--help" || string( argv[a]) == "-h") {
+      cerr << "Usage: cat xxx.pro | demo [-options]" << endl;
+      cerr << "options:" << endl;
+      cerr << "  --help (-h)        display this message" << endl;
+      cerr << "  --tree (-t)    show tree" << endl;
+      cerr << "  --token-only (-x)  compute only tokens (speed test for lexer)" << endl;
+      cerr << "  --tokens (-o)    show tokens" << endl;
+      return 0;
+      }
+      if (string(argv[a])=="--tree" || string(argv[a])=="-t" || string(argv[a])=="-T")
+	{
+	  showtree=true;
+	}
+      if (string(argv[a])=="--tokens" || string(argv[a])=="-o" || string(argv[a])=="-O")
+	{
+	  showtokens=true;
+	} 
+      if (string(argv[a])=="--token-only" || string(argv[a])=="-x" || string(argv[a])=="-X")
+	{
+	  token_only=true;
+	} 
+   }
+    
   ANTLRInputStream * input = new ANTLRInputStream(std::cin); // read stdin
   gdlLexer *lexer = new gdlLexer(input);
   CommonTokenStream *tokens = new CommonTokenStream(lexer);
-
-  //  tokens->fill();
-//  for (auto token : tokens->getTokens()) {
-//        std::cout << token->toString() << std::endl;
-//  }
-//
+  if (token_only) {
+    tokens->fill();
+    if (showtokens) {
+      for (auto token : tokens->getTokens()) std::cout << token->toString() << std::endl;
+    }
+    return 0;
+  }
+  
   gdlParser * parser=new gdlParser(tokens);
   tree::ParseTree * tree = parser->translation_unit();
 
-  //  std::cout << tree->toStringTree(&parser) << std::endl << std::endl;
+  if (showtokens) {
+    for (auto token : tokens->getTokens()) std::cout << token->toString() << std::endl;
+  }
+  if (showtree) std::cout << tree->toStringTree(&parser) << std::endl << std::endl;
 
   return 0;
 }
