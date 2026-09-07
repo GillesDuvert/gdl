5f49f3e0a613f3e4b18e75c5df4d02bfd96a1250
Giloo <gildas@localhost>
2026-09-07 23:42:19 +0200

    correcting code issuing warnings on OSX

--- src/export.hpp
+++ src/export.hpp
@@ -355,14 +355,14 @@ std::string Backtrace(int skip = 1)
 			char *demangled = NULL;
 			int status;
 			demangled = abi::__cxa_demangle(info.dli_sname, NULL, 0, &status);
-			snprintf(buf, sizeof(buf), "%-3d %0*x %s + %zd\n",
-					 i, 2 + (int)(sizeof(void*)) * 2, callstack[i],
+			snprintf(buf, sizeof(buf), "%-3d %0*llx %s + %zd\n",
+					 i, 2 + sizeof(void*) * 2, callstack[i],
 					 status == 0 ? demangled : info.dli_sname,
 					 (char *)callstack[i] - (char *)info.dli_saddr);
 			free(demangled);
 		} else {
-			snprintf(buf, sizeof(buf), "%-3d %0*x\n",
-					 i, 2 + (int)(sizeof(void*)) * 2, callstack[i]);
+			snprintf(buf, sizeof(buf), "%-3d %0*llx\n",
+					 i, 2 + sizeof(void*) * 2, callstack[i]);
 		}
 		trace_buf << buf;
 
